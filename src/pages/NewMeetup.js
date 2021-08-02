f90a8bfe1f0686f617dee86ba8bfe19b3cfeb2bf
popov986 <41522818+popov986@users.noreply.github.com>
2021-08-02 17:10:12 +0200

    first commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,26 @@
+import NewMeetupForm from './../components/meetups/NewMeetupForm';
+
+
+
+function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+      fetch(
+          'https://react-meetup-cdb96-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+          {
+              method: 'POST',
+              body: JSON.stringify(meetupData),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+          }
+      );
+    } 
+
+
+    return <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </section>
+}
+
+export default NewMeetupPage;
